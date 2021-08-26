6291b4cec70e4249f2e0bed8ce18eb6847091442
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-08-26 10:02:39 +0430

    fic linters issue

--- src/js/index.js
+++ src/js/index.js
@@ -1,45 +1,22 @@
-import _ from 'lodash';
-import '../css/style.css'
-const todos=[{
-  index:4,
-  description: "todo 4",
-  completed: true
+import '../css/style.css';
+
+const todos = [{
+  index: 4,
+  description: 'todo 4',
+  completed: true,
 },
 {
-  index:1,
-  description: " todo 1",
-  completed: false
+  index: 1,
+  description: ' todo 1',
+  completed: false,
 },
 {
-  index:3,
-  description: " todo 3",
-  completed: true
+  index: 3,
+  description: ' todo 3',
+  completed: true,
 },
-]
-function displayBooks() {
-  sortBooks();
-  const section = document.getElementById('todos');
-  const list = document.createElement('ul');
-  list.id = 'list';
-  todos.forEach((todo) => {
-    const {description,completed,index} = todo;
-    const liId = `li${index}`;
-    let todoCard = `<li id=${liId} class= "todo" >`;
-      if(completed){
-        todoCard=todoCard+'<input type="checkbox" checked>'
-      }
-      else{
-        todoCard=todoCard+'<input type="checkbox">'
-      }
-      todoCard=todoCard+ `<p>${description}</p>
-      </li>
-      <hr>`;
-    list.insertAdjacentHTML('beforeend', todoCard);
-  });
-  section.innerHTML = '';
-  section.appendChild(list);
-}
-function sortBooks(){
+];
+function sortBooks() {
   todos.sort((todoA, todoB) => {
     if (todoA.index < todoB.index) {
       return -1;
@@ -50,4 +27,26 @@ function sortBooks(){
     return 0;
   });
 }
-displayBooks();
+function displayBooks() {
+  sortBooks();
+  const section = document.getElementById('todos');
+  const list = document.createElement('ul');
+  list.id = 'list';
+  todos.forEach((todo) => {
+    const { description, completed, index } = todo;
+    const liId = `li${index}`;
+    let todoCard = `<li id=${liId} class= "todo" >`;
+    if (completed) {
+      todoCard += '<input type="checkbox" checked>';
+    } else {
+      todoCard += '<input type="checkbox">';
+    }
+    todoCard += `<p>${description}</p>
+    </li>
+    <hr>`;
+    list.insertAdjacentHTML('beforeend', todoCard);
+  });
+  section.innerHTML = '';
+  section.appendChild(list);
+}
+displayBooks(); 
